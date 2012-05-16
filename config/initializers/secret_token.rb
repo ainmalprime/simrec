2a7c047700a96b92f13df3c16a11196cc8a3e666
Thomas Giguiere <thomas@whitecoattech.com>
2012-05-16 08:57:21 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeVryEHR::Application.config.secret_token = '164dff27b3a251a4db9051ed25316454f2af1e0ec887d8a8ba94c99c810c1b0e00535335d795f912e9250cc6e34d96a756a4466e3937bb3f7a96c5d8e8781bc7'
