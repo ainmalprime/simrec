cae6c6ff7965228cbbfa0922de65e14a11f47fd2
Thomas Giguiere <thomas@whitecoattech.com>
2012-05-19 12:43:39 -0700

    attempting to fix visittime case sensitive issue 3

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -1,10 +1,10 @@
 class Visit < ActiveRecord::Base
-  	attr_accessible :chiefComplaint, :dischargeDiagnosis, :isActiveSim, :mrNumber, :patient_id, :visitTime
+  	attr_accessible :chiefComplaint, :dischargeDiagnosis, :isActiveSim, :mrNumber, :patient_id, :visittime
    	validates :patient_id, presence: true
   	belongs_to :patient
   	has_many :clinician_notes
  
- 	default_scope order: 'visits.visitTime'
+ 	default_scope order: 'visits.visittime'
 
 
 end
