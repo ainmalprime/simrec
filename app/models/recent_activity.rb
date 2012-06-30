da8a57ffeff2fa9bad38b616a18b8a70422a1060
Thomas Giguiere <thomas@whitecoattech.com>
2012-06-29 23:00:02 -0400

    recent activity

--- app/models/recent_activity.rb
+++ app/models/recent_activity.rb
@@ -1,3 +1,3 @@
 class RecentActivity < ActiveRecord::Base
-  attr_accessible :description, :resource, :resource_id, :time_recorded, :visit_id
+  attr_accessible :description, :resource, :resource_id, :time_recorded, :visit_id, :visible
 end
